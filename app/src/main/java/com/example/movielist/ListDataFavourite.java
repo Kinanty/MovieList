be6350b92b3ecebd71acbdb331659902fa723d09
Kinanty <kinantykusuma@gmail.com>
2020-10-23 23:44:26 +0700

    update

--- app/src/main/java/com/example/movielist/ListDataFavourite.java
+++ app/src/main/java/com/example/movielist/ListDataFavourite.java
@@ -39,7 +39,11 @@ public class ListDataFavourite extends AppCompatActivity {
             public void onClick(int position) {
                 // intent ke detail movie
                 Intent move = new Intent(getApplicationContext(), DetailFavourite.class);
-                move.putExtra("title", DataArrayList.get(position).getJudul());
+                move.putExtra("judul",DataArrayList.get(position).getJudul());
+                move.putExtra("path",DataArrayList.get(position).getPath());
+                move.putExtra("date",DataArrayList.get(position).getReleaseDate());
+                move.putExtra("deskripsi",DataArrayList.get(position).getDesc());
+
                 startActivity(move);
             }
 
