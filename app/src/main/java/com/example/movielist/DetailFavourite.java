5f90900756083bc6bc2c78d7378b0933c6e36851
Kinanty <kinantykusuma@gmail.com>
2020-10-31 21:41:10 +0700

    update

--- app/src/main/java/com/example/movielist/DetailFavourite.java
+++ app/src/main/java/com/example/movielist/DetailFavourite.java
@@ -3,15 +3,48 @@ package com.example.movielist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.Target;
 
 public class DetailFavourite extends AppCompatActivity {
+    Bundle extras;
+    String title;
+    String date;
+    String deskripsi;
+    String path;
+    String id;
+
+    TextView tvjudul;
+    ImageView ivposter;
+    TextView tvdesc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_favourite);
+        extras = getIntent().getExtras();
+        tvjudul = (TextView)findViewById(R.id.tvjudul);
+        tvdesc = (TextView)findViewById(R.id.txtdeskripsi);
+        ivposter = (ImageView) findViewById(R.id.ivposter);
         //get extra
-
+        if (extras != null) {
+            title = extras.getString("judul");
+            id = extras.getString("id");
+            date = extras.getString("date");
+            deskripsi = extras.getString("deskripsi");
+            path = extras.getString("path");
+            tvjudul.setText(title);
+            tvdesc.setText(deskripsi);
+            Glide.with(DetailFavourite.this)
+                    .load(path)
+                    .override(Target.SIZE_ORIGINAL)
+                    .placeholder(R.mipmap.ic_launcher)
+                    .into(ivposter);
+            // and get whatever type user account id is
+        }
         //tampilkan data di sini
     }
 }
